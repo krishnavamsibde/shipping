9fe47b05a04064c7e1839f2ba4de3bd2347fe6cc
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-28 00:33:56 -0700

    updating ./roboshop-source-code/shipping

---
+++ ami.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-kvbucket"
+    key    = "ami/shipping/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+module "ami" {
+  source = "github.com/krishnavamsi7616/immutable-app-ami"
+  COMPONENT = "shipping"
+  APP_VERSION = var.APP_VERSION
+}
+
+variable "APP_VERSION" {}
