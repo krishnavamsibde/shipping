73219e0e1d797e97ff50841964ad81dbde92a0e4
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-09-06 23:12:03 -0700

    updating ./roboshop-source-code/shipping

--- ami.tf
+++ ami.tf
@@ -14,6 +14,7 @@ module "ami" {
   source = "github.com/krishnavamsi7616/immutable-app-ami"
   COMPONENT = "shipping"
   APP_VERSION = var.APP_VERSION
+  VPC_ID      = var.VPC_ID
 }
 
 variable "APP_VERSION" {}
